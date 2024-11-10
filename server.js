68be8a3b191db61e93619695d84fcf25781d3f15
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2024-11-10 13:59:55 +0700

    test

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express()
 const port = 8000
 
 
-
-
-
-
 app.listen(port,(req,res)=>{
     console.log("Server started at port",port)
 })
+
+app.get('/get',(req,res)=>{
+    res.json({
+        message:"helloworld"
+    })
+})
+
